b183763707aaaa404990cd01eb44a87d825a38a0
BrandyBecker <brandybeckerfsw@gmail.com>
2020-01-19 13:54:50 -0500

    Working on Bio Section

--- beccas-website/src/App.js
+++ beccas-website/src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from '../src/Components/NavBar.js';
 import MiddleAboutSection from '../src/Components/MiddleAboutSection.js'
+import BioSection from '../src/Components/BioSection.js'
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <MiddleAboutSection/>
+      <BioSection/>
     </div>
   );
 }
---
+++ beccas-website/src/Components/BioSection.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../App.css'
+
+let BioSection = () => {
+
+    return (
+        <div class="bio-section">
+           <img class="beximg" src="https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/80874700_2444702225769576_4840277685592653824_n.jpg?_nc_cat=108&_nc_ohc=8Ej9yDvHdN8AX_zhgY1&_nc_ht=scontent-ort2-1.xx&oh=2f349bd8635bd5286ad4b094f7e5cd31&oe=5E9F6DCC"/>
+           
+           <div class="bio-about">
+               <h3> About Me: </h3>
+               <p>
+                    Star jupiter experience jupiter spiritual pleiades alien matrix. Natural aliens wave wave moon spirit consciousness. Moon orbit moon cognitive mythology alien, vibration energy emotional mass galactic emotional. Consciousness cognitive experience solar spaceship hubble, magic good vibration good spacetime century. Time hinduism wave, light materialist gaia aliens meaning century concepts wavelength materialist spatial wavelength.
+               </p>
+           </div>
+        </div>
+    )
+}
+
+export default BioSection;
