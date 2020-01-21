3bddc39611c8f20b953c9170d2851675cd60581c
BrandyBecker <brandybeckerfsw@gmail.com>
2020-01-20 22:44:38 -0500

    small changes to css

--- beccas-website/src/Components/Contact.js
+++ beccas-website/src/Components/Contact.js
@@ -6,17 +6,19 @@ function Contact() {
    <div class="contact-form" id="contact_form">
        <form action="mailto:brandybeckerfsw@gmail.com" method="post" enctype="text/plain">
             <div class="input-form">
-                <input type="text" name="name" id="name" placeholder="Your Name" />
+                <input type="text" name="My_Name" id="name" placeholder="Your Name" />
             </div>
             <div class="input-form">
-                <input type="text" name="email" id="email" placeholder="Your E-Mail" />
+                <input type="text" name="My_Email" id="email" placeholder="Your E-Mail" />
             </div>
             <div class="input-form">
-                <textarea  name="message" id="message" defaultValue="I would like to set up a time for a Tarot Reading! ✨"></textarea>
+                <textarea  name="Tarot_Reading_Message" id="message" defaultValue="I would like to set up a time for a Tarot Reading! ✨"></textarea>
             </div>
             <div class="input-form">
                 <input class="contact-button" type="submit" value="Contact Me! 🔮" />
             </div>
+
+
 		</form>						
    </div>
   );
