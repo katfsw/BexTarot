5e052c614b01b6caf2d3d2145bb327116eca724b
BrandyBecker <brandybeckerfsw@gmail.com>
2020-02-27 20:28:44 -0500

    Updates to Readings

--- beccas-website/src/Components/BioSection.js
+++ beccas-website/src/Components/BioSection.js
@@ -4,7 +4,7 @@ let BioSection = () => {
 
     return (
         <div class="bio-section" id="about_me">
-           <img class="beximg" src="https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/80874700_2444702225769576_4840277685592653824_n.jpg?_nc_cat=108&_nc_ohc=8Ej9yDvHdN8AX_zhgY1&_nc_ht=scontent-ort2-1.xx&oh=2f349bd8635bd5286ad4b094f7e5cd31&oe=5E9F6DCC"/>
+           <img class="beximg" alt="Rebecca, Tarot Reader" src="https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/80874700_2444702225769576_4840277685592653824_n.jpg?_nc_cat=108&_nc_ohc=8Ej9yDvHdN8AX_zhgY1&_nc_ht=scontent-ort2-1.xx&oh=2f349bd8635bd5286ad4b094f7e5cd31&oe=5E9F6DCC"/>
            
            <div class="bio-about">
                <h3> About Me: </h3>
--- beccas-website/src/Components/Readings.js
+++ beccas-website/src/Components/Readings.js
@@ -2,32 +2,52 @@ import React from 'react'
 
 const Readings = () => {
     return(
-        <div>
+        <div className="readings-spreads">
+          
+          <h1>One-Time Purchase</h1>
             <div className="single-spreads">
 
-                <div>
+                
+
+                <div className="single-card">
                     <h3>1 Card Spread</h3>
-                    <img src={require('../images/SingleCard.png')} alt='Single Card Tarot Spread'/>
+                    <img src={require('../images/SingleCardV2.png')} alt='Single Card Tarot Spread'/>
                     <p>$2.50</p>
                 </div> 
 
                 <div>
                     <h3>3 Card Spread</h3>
-                    <img src={require('../images/ThreeCard.png')} alt='Three Card Tarot Spread'/>
+                    <img src={require('../images/ThreeCardV2.png')} alt='Three Card Tarot Spread'/>
                     <p>$8</p>
                 </div> 
 
                 <div>
                     <h3>8 Card Spread</h3>
-                    <img src={require('../images/EightCard.png')} alt='Eight Card Tarot Spread'/>
+                    <img src={require('../images/EightCardV2.png')} alt='Eight Card Tarot Spread'/>
                     <p>$15</p>
                 </div> 
 
                 
             </div>
+            
+            <h1>One-Month Subscription</h1>
             <div className="monthly-spreads">
                 
-            </div>
+                <div>
+                    <h3>3 Card Spread</h3>
+                    <img src={require('../images/ThreeCardV2.png')} alt='Monthly Three Card Tarot Spread'/>
+                    <p>$32</p>
+                    <p>* 4 Readings in a Month, every Wednesday</p>
+                </div> 
+
+                <div>
+                    <h3>8 Card Spread</h3>
+                    <img src={require('../images/EightCardV2.png')} alt='Monthly Eight Card Tarot Spread'/>
+                    <p>$60</p>
+                    <p>* 4 Readings in a Month, every Wednesday</p>
+                </div> 
+
+            </div> 
         </div>
     )   
 }
